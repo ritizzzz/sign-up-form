6bf02e6ff228d73a72d7f809dce0733c218e1c60
ritizzzz <kandelritiz@gmail.com>
2022-06-24 21:55:02 +1000

    simple font styling

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ document.querySelector('#confirmpassword').addEventListener('keyup', ()=>{
 })
 
 document.querySelector('#password').addEventListener('keyup', ()=>{
-if(confirmpassword.value !== ''){
-    toggleMessage();
-}
+    if(confirmpassword.value !== ''){
+        toggleMessage();
+    }
 })
 
 function toggleMessage(){
 console.log(confirmpassword.value == password.value);
-if(password.value === confirmpassword.value ){
-    errorMessage.classList.remove('erron');
-    button.disabled = false;
-}else{
-    errorMessage.classList.add('erron')
-    button.disabled = true;
-}
+    if(password.value === confirmpassword.value ){
+        password.style.border = '1px solid #E5E7EB';
+        errorMessage.classList.remove('erron');
+        button.disabled = false;
+    }else{
+        password.style.border = '1px solid red';
+        errorMessage.classList.add('erron');
+        button.disabled = true;
+    }
 }
